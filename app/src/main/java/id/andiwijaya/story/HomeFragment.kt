2f74e15d5bf45e3e235c026be0e4708174306671
Andi Wijaya <andiwijayaid@gmail.com>
2022-10-09 23:52:44 +0800

    create initial view

---
+++ app/src/main/java/id/andiwijaya/story/HomeFragment.kt
@@ -0,0 +1,36 @@
+package id.andiwijaya.story
+
+import android.os.Bundle
+import android.util.Log
+import android.view.*
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import id.andiwijaya.story.databinding.FragmentHomeBinding
+
+class HomeFragment: Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.toolbar.addMenuProvider(object : MenuProvider{
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_home, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                Log.d("Log", "TODO")
+                return true
+            }
+        })
+    }
+
+}
--- app/src/main/java/id/andiwijaya/story/MainActivity.kt
+++ app/src/main/java/id/andiwijaya/story/MainActivity.kt
@@ -1,11 +1,26 @@
 package id.andiwijaya.story
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.andiwijaya.story.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val host: NavHostFragment = supportFragmentManager
+//            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+//
+//        val navController = host.navController
+//        setupActionBarWithNavController(navController)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            window.statusBarColor = Color.BLACK
+        }
     }
+
 }
