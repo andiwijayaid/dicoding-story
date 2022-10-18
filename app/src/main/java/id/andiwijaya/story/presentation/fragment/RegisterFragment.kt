09b42c9c36f8759ddebea3ea7b02f64ba1432463
Andi Wijaya <andiwijayaid@gmail.com>
2022-10-18 18:56:05 +0800

    add register

---
+++ app/src/main/java/id/andiwijaya/story/presentation/fragment/RegisterFragment.kt
@@ -0,0 +1,23 @@
+package id.andiwijaya.story.presentation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import id.andiwijaya.story.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
